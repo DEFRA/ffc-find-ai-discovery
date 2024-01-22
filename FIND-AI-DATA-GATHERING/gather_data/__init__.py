b781aa6d5c9cb96425ce06519f9e71ca28328443
johnmctiernan <johnmctiernan98@gmail.com>
2024-01-22 14:53:23 +0000

    Fixed incorrect code indentation in __init__.py

--- FIND-AI-DATA-GATHERING/gather_data/__init__.py
+++ FIND-AI-DATA-GATHERING/gather_data/__init__.py
@@ -18,24 +18,23 @@ def main(mytimer: TimerRequest) -> None:
     if mytimer.past_due:
         logging.info('The timer is past due!')
         
-        all_links = get_all_links_from_reference(base_url)
-        for link in all_links:
-            doc_title, webpage_content = create_documents_from_webpage(link)
+    all_links = get_all_links_from_reference(base_url)
+    for link in all_links:
+        doc_title, webpage_content = create_documents_from_webpage(link)
+        
+        if check_data_freshness(webpage_content, doc_title):
+            webpage_data = str(link) + "\n" + webpage_content
+            webpage_data = webpage_data.encode('utf-8')
+            webpage_file_name = str(doc_title) + ".txt"
             
-            if check_data_freshness(webpage_content, doc_title):
-                webpage_data = str(link) + "\n" + webpage_content
-                webpage_data = webpage_data.encode('utf-8')
-                webpage_file_name = str(doc_title) + ".txt"
-                
-                blob_client = blob_service_client.get_blob_client(container = CONTAINER_NAME_STRING,
-                                                                    blob = webpage_file_name)
-                
-                print("\nUploading to Azure Storage as blob:\n\t" + webpage_file_name)
-                
-                blob_client.upload_blob(webpage_data, overwrite = True)
-            else:
-                print ("\n"+ str(doc_title) + ' is already up-to-date.')
-    else:
-        logging.info('Code did not run as timer is not past due')
+            blob_client = blob_service_client.get_blob_client(container = CONTAINER_NAME_STRING,
+                                                                blob = webpage_file_name)
+            
+            logging.info("\nUploading to Azure Storage as blob:\n\t" + webpage_file_name)
+            
+            blob_client.upload_blob(webpage_data, overwrite = True)
+        else:
+            logging.info("\n"+ str(doc_title) + ' is already up-to-date.')
+    
 
     logging.info('Python timer trigger function ran at %s', utc_timestamp)
