902e843b20b6f9e875b5787bc38336c3b85bfb89
johnmctiernan <johnmctiernan98@gmail.com>
2024-01-25 12:58:43 +0000

    Fix for doc_title metadata including filename

--- FIND-AI-DATA-GATHERING/page_chunk_func/__init__.py
+++ FIND-AI-DATA-GATHERING/page_chunk_func/__init__.py
@@ -22,9 +22,8 @@ def main(myblob: InputStream):
                 
                 logging.info("\nUploading Chunk to Azure Storage as blob:\n\t" + chunk[0])
                 
-                blob_prefix = f"b'{INPUT_CONTAINER_NAME}/"
                 blob_title = blob_name.replace(".txt'", "")
-                blob_title = blob_title.replace(blob_prefix, "")
+                blob_title = blob_title.replace((INPUT_CONTAINER_NAME + "/"), "")
                 
                 
                 blob_title = blob_title.encode('utf-8')
