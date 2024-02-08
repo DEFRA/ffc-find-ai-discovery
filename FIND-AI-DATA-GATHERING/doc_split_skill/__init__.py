43c986e56ab733648313c8b0b0b7c4418727755b
johnmctiernan <johnmctiernan98@gmail.com>
2024-02-08 17:04:39 +0000

    added try except to catch error in doc split skill

--- FIND-AI-DATA-GATHERING/doc_split_skill/__init__.py
+++ FIND-AI-DATA-GATHERING/doc_split_skill/__init__.py
@@ -17,65 +17,68 @@ def main(req: HttpRequest) -> HttpResponse:
             pass
         else:
             values = req_body.get('values') 
-
-    data = values[0]['data']
-    record_id = values[0]["recordId"]
-    document_id = data["document_id"]
-    
-    text = data["text"]
-    filename = data["filename"]
-    
-    logging.info(filename)
-    logging.info(document_id)
-    
-    doc_chunks, doc_url = split_content_by_headings(str(filename), str(text))
-    
-    blob_title = filename.replace(".txt'", "")
-    blob_title = blob_title.replace((INPUT_CONTAINER_NAME + "/"), "")
-    
-    
-    current_datetime = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
-    
-    chunks_list = []
-    
-    for idx, chunk in enumerate(doc_chunks):
+            
+    try:   
+        data = values[0]['data']
+        record_id = values[0]["recordId"]
+        document_id = data["document_id"]
         
-        section_file_name = chunk[0].split(' -- ')[1]
-        chunk_title = section_file_name.replace('.txt', '') 
-    
-        chunk_identifier = f"(Title: {blob_title} | Source: {doc_url} | Section: {chunk_title})==="
-    
-        output_content = chunk_identifier + chunk[1]
+        text = data["text"]
+        filename = data["filename"]
         
-        chunkJson = {
-            "content": output_content,
-            "chunk_id": str(idx),
-            "title": chunk[0],
-            "filepath": filename,
-            "source_url": doc_url,
-            "last_updated": current_datetime
+        logging.info(filename)
+        logging.info(document_id)
+        
+        doc_chunks, doc_url = split_content_by_headings(str(filename), str(text))
+        
+        blob_title = filename.replace(".txt'", "")
+        blob_title = blob_title.replace((INPUT_CONTAINER_NAME + "/"), "")
+        
+        
+        current_datetime = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+        
+        chunks_list = []
+        
+        for idx, chunk in enumerate(doc_chunks):
+            
+            section_file_name = chunk[0].split(' -- ')[1]
+            chunk_title = section_file_name.replace('.txt', '') 
+        
+            chunk_identifier = f"(Title: {blob_title} | Source: {doc_url} | Section: {chunk_title})==="
+        
+            output_content = chunk_identifier + chunk[1]
+            
+            chunkJson = {
+                "content": output_content,
+                "chunk_id": str(idx),
+                "title": chunk[0],
+                "filepath": filename,
+                "source_url": doc_url,
+                "last_updated": current_datetime
+            }
+            
+            chunks_list.append(chunkJson)
+            
+        
+        response = {"values": [
+            {
+                "recordId": record_id,
+                "data": {
+                    "chunks": chunks_list,
+                    "total_files": len(chunks_list),
+                    "num_unsupported_format_files": 0,
+                    "num_files_with_errors": 0,
+                    "skipped_chunks": 0
+                },
+                "errors": None,
+                "warnings": None
+            }
+        ]
         }
         
-        chunks_list.append(chunkJson)
-        
-    
-    response = {"values": [
-        {
-            "recordId": record_id,
-            "data": {
-                "chunks": chunks_list,
-                "total_files": len(chunks_list),
-                "num_unsupported_format_files": 0,
-                "num_files_with_errors": 0,
-                "skipped_chunks": 0
-            },
-            "errors": None,
-            "warnings": None
-        }
-    ]
-    }
-    
-    return HttpResponse(
-        json.dumps(response),
-        mimetype="application/json"
-    )
+        return HttpResponse(
+            json.dumps(response),
+            mimetype="application/json"
+        )
+    except Exception as error:
+        return HttpResponse(error)
